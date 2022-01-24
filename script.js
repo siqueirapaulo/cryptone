bc8d54818099f093870c307001b8787b8f2ff53a
Paulo Siqueira <pvictor_ps@hotmail.com>
2022-01-23 21:55:48 -0400

    melhoria do código JS

--- script.js
+++ script.js
@@ -1,161 +1,93 @@
-// VARIÁVEIS
+// VARIÁVEIS GLOBAIS ==========================================================
 
-var originalMessage = document.querySelector("#original-message");
-var resultMessage = document.querySelector("#result");
-var errorMessage = document.querySelector("#error-message");
+var originalTextarea = document.querySelector("#original-message");
+var resultTextarea = document.querySelector("#result");
+
+var warnMessage = document.querySelector("#warn-message");
 
 var cryptButton = document.querySelector("#crypt-button");
 var decryptButton = document.querySelector("#decrypt-button");
+var copyButton = document.querySelector("#copy-button");
 
 // FUNÇÃO DE CRIPTOGRAFIA
 
 cryptButton.onclick = function() {
-    var original = originalMessage.value;
-    var crypted = "";
-    var error = false;
+    var originalMessage = originalTextarea.value; // Mensagem a ser criptografada.
+    var cryptedMessage; // Mensagem criptografada.
+    var warnIcon = "<img id='warn-icon' src='images/warn.png'>";
+    var errorMessage = "<p>Não foi possível criptografar. A mensagem deve possuir somente letras minúsculas e sem acento.</p>";
 
-    for (i = 0; i < original.length; i++) {
-        if (original[i] != " ") {
-            if (original[i].codePointAt(0) < 97 || original[i].codePointAt(0) > 122) {
-                error = true;
-                i = original.length;
-            }
-        }
-
-        switch (original[i]) {
-            case ('a'):
-                crypted += "ai";
-                break;
-            
-            case ('e'):
-                crypted += "enter";
-                break;
-        
-            case ('i'):
-                crypted += "imes";
-                break;
-            
-            case ('o'):
-                crypted += "ober";
-                break;
-    
-            case ('u'):
-                crypted += "ufat";
-                break;
-    
-            default:
-                crypted += original[i];
-        }
-    }
-
-    if (error) {
-        resultMessage.innerHTML = "";
-        errorMessage.innerHTML = "<img id='warn-icon' src='images/warn.png'><p>Não foi possível criptografar. A mensagem deve possuir somente letras minúsculas e sem acento.</p>";
+    // Verificando a existência de letras maiúsculas, números, acentos e caracteres especiais.
+    if (originalMessage.match(/[A-Z0-9À-ü\W]/)) {
+        warnMessage.innerHTML = `${warnIcon} ${errorMessage}`;
+        resultTextarea.value = "";
     } else {
-        resultMessage.innerHTML = crypted;
-        errorMessage.innerHTML = "";
+        // Criptografando a mensagem.
+        cryptedMessage = originalMessage.replace(/e/g, "enter");
+        cryptedMessage = cryptedMessage.replace(/i/g, "imes");
+        cryptedMessage = cryptedMessage.replace(/a/g, "ai");
+        cryptedMessage = cryptedMessage.replace(/o/g, "ober");
+        cryptedMessage = cryptedMessage.replace(/u/g, "ufat");
+
+        resultTextarea.value = cryptedMessage;
+        warnMessage.innerHTML = "";
     }
+
+    /*if (!originalMessage.match(/ai|enter|imes|ober|ufat/)) {
+        cryptedMessage = originalMessage.replace(/e/g, "enter");
+        cryptedMessage = cryptedMessage.replace(/i/g, "imes");
+        cryptedMessage = cryptedMessage.replace(/a/g, "ai");
+        cryptedMessage = cryptedMessage.replace(/o/g, "ober");
+        cryptedMessage = cryptedMessage.replace(/u/g, "ufat");
+
+        resultTextarea.value = cryptedMessage;
+        warnMessage.innerHTML = "";
+    } else {
+        warnMessage.innerHTML = "<img id='warn-icon' src='images/warn.png'>" +
+        "<p>Não foi possível criptografar. A mensagem já está criptografada.</p>";
+    
+        resultTextarea.value = "";
+    }*/
 }
 
-// FUNÇÃO DE DESCRIPTOGRAFIA
+// FUNÇÃO DE DESCRIPTOGRAFIA ==================================================
 
 decryptButton.onclick = function() {
-    var original = originalMessage.value;
-    var decrypted = "";
-    var error = false;
-    var i = 0;
-
-    while (i < original.length) {
-        switch (original[i]) {
-            case ('a'):
-                if (original[i+1] == 'i') {
-                    decrypted += original[i];
-                    i += 2;
-                } else {
-                    error = true;
-                    i = original.length;
-                }
-
-                break;
-            
-            case ('e'):
-                if ((original[i+1] == 'n') &&
-                    (original[i+2] == 't') &&
-                    (original[i+3] == 'e') &&
-                    (original[i+4] == 'r'))
-                {
-                    decrypted += original[i];
-                    i += 5;
-                } else {
-                    error = true;
-                    i = original.length;
-                }
-
-                break;
-        
-            case ('i'):
-                if ((original[i+1] == 'm') &&
-                    (original[i+2] == 'e') &&
-                    (original[i+3] == 's'))
-                {
-                    decrypted += original[i];
-                    i += 4;
-                } else {
-                    error = true;
-                    i = original.length;
-                }
-
-                break;
-            
-            case ('o'):
-                if ((original[i+1] == 'b') &&
-                    (original[i+2] == 'e') &&
-                    (original[i+3] == 'r'))
-                {
-                    decrypted += original[i];
-                    i += 4;
-                } else {
-                    error = true;
-                    i = original.length;
-                }
-                
-                break;
-
-            case ('u'):
-                if ((original[i+1] == 'f') &&
-                    (original[i+2] == 'a') &&
-                    (original[i+3] == 't'))
-                {
-                    decrypted += original[i];
-                    i += 4;
-                } else {
-                    error = true;
-                    i = original.length;
-                }
-                
-                break;
-
-            default:
-                decrypted += original[i];
-                i++;
-        }
-    }
-
-    if (error) {
-        errorMessage.innerHTML = "<img id='warn-icon' src='images/warn.png'><p>Não foi possível descriptografar. A mensagem não está criptografada corretamente.</p>";
-        resultMessage.innerHTML = "";
-        error = false;
+    var originalMessage = originalTextarea.value; // Mensagem a ser descriptografada.
+    var decryptedMessage; // Mensagem descriptografada.
+    var warnIcon = "<img id='warn-icon' src='images/warn.png'>";
+    var errorMessage = "<p>A mensagem não está criptografada.</p>";
+    
+    // Verificando a existência das chaves criptográficas.
+    if (!originalMessage.match(/ai|enter|imes|ober|ufat/)) {
+        warnMessage.innerHTML = `${warnIcon} ${errorMessage}`;
     } else {
-        resultMessage.innerHTML = decrypted;
-        errorMessage.innerHTML = "";
+        warnMessage.innerHTML = "";
     }
+
+    // Descriptografando a mensagem.
+    decryptedMessage = originalMessage.replace(/ai/g, 'a');
+    decryptedMessage = decryptedMessage.replace(/enter/g, 'e');
+    decryptedMessage = decryptedMessage.replace(/imes/g, 'i');
+    decryptedMessage = decryptedMessage.replace(/ober/g, 'o');
+    decryptedMessage = decryptedMessage.replace(/ufat/g, 'u');
+
+    resultTextarea.value = decryptedMessage;
 }
 
-// FUNÇÃO DE COPIAR PARA O CLIPBOARD
-
-var copyButton = document.querySelector("#copy-button");
+// FUNÇÃO DE COPIAR PARA O CLIPBOARD ==========================================
 
 copyButton.onclick = function() {
-    var copyMessage = document.querySelector("#result").innerHTML;
-    navigator.clipboard.writeText(copyMessage);
+    var copyMessage = document.querySelector("#result").value;
+    var warnIcon = "<img id='warn-icon' src='images/copy.png'>";
+    var notifyMessage = "<p>Mensagem copiada com sucesso!</p>";
+
+    navigator.clipboard.writeText(copyMessage); // Copia a mensagem para o clipboard.
+
+    warnMessage.innerHTML = `${warnIcon} ${notifyMessage}`;
+
+    // Delay de 5s para o alerta sumir.
+    setTimeout(function() {
+        warnMessage.innerHTML = "";
+    }, 5000)
 }
